0b8c54cfb291ab20f02d1a433d974c69de65dd94
sergiu <sergiu.g@appwrk.com>
2022-12-09 16:58:54 +0530

    change port number

--- app.js
+++ app.js
@@ -11,14 +11,15 @@ app.use(bodyParser.json())
 app.use(cors());
 
 app.use("/",route);
+const PORT = process.env.PORT || 5000;
 mongoose.set('strictQuery', true);
 mongoose
   .connect(
     "mongodb+srv://helder:helder@cluster0.1omlqxj.mongodb.net/?retryWrites=true&w=majority"
   )
   .then(() => {
-    app.listen(5000,()=>{
-        console.log("connect successfully")
+    app.listen(PORT,()=>{
+        console.log(`connect successfully on port ${PORT}`);
     })
   })
   .catch((err) => {
